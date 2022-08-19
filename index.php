9a74e866959f8bb394031142145ec9f719593ea2
Erick Treichel <106178471+ericktreichel@users.noreply.github.com>
2022-08-18 21:51:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,9 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "44.204.26.95";
 $username = "root";
-$password = "Senha123";
+$password = "capivara12345";
 $database = "meubanco";
 
 // Criar conexão
@@ -29,16 +29,20 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-$valor_rand1 =  rand(1, 999);
+$valor_rand1 =  rand(100, 999);
 $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$valor_rand3 = strtoupper(substr(str_shuffle('ABCADEOFGAHAEIOSCPRMNEIJICELMORAEIONMIAOPSORQAEIOREOSTNPSLUVWACXMSOIYZ'),>
 $host_name = gethostname();
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand3', '$valor_rand3', '$valor_rand2', '$valor_rand3', $host_name)";
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+  echo "<br>New record successfully added to the " . $database . " database.<br>";
+  echo "<br> Entry added:<br>";
+  echo "AlunoID: " . $valor_rand1 . "<br>";
+  echo "Nome completo: " . $valor_rand3 . "<br>";
+  echo "Endereco: " . $valor_rand2 . "<br>";
+  echo "Cidade: " . $valor_rand3 . "<br>";
 } else {
   echo "Error: " . $link->error;
 }
